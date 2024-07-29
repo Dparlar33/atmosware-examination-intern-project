6c94eb034ea0dc2004fde93ab6be03f6b4411b72
denizparlar <dparlar23@gmail.com>
2024-07-29 17:18:38 +0300

    management service and auth operations deleted

--- managementService/src/main/java/com/atmosware/managementService/dataAccess/OrganizationRepository.java
+++
@@ -1,9 +0,0 @@
-package com.atmosware.managementService.dataAccess;
-
-import com.atmosware.managementService.entities.Organization;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import java.rmi.server.UID;
-
-public interface OrganizationRepository extends JpaRepository<Organization, UID> {
-}
