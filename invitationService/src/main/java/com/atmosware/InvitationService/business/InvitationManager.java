4c0ce5f5583a2899c839e9f7e10d0e22152ca97d
denizparlar <dparlar23@gmail.com>
2024-08-03 18:20:17 +0300

    real email sending configuration added

--- invitationService/src/main/java/com/atmosware/InvitationService/business/InvitationManager.java
+++ invitationService/src/main/java/com/atmosware/InvitationService/business/InvitationManager.java
@@ -5,6 +5,8 @@ package com.atmosware.InvitationService.business;
 import com.atmosware.InvitationService.core.LoggerService;
 import com.atmosware.common.invitation.InvitationRequest;
 import lombok.RequiredArgsConstructor;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -12,9 +14,17 @@ import org.springframework.stereotype.Service;
 public class InvitationManager implements InvitationService {
 
     private final LoggerService logger;
+    private final JavaMailSender javaMailSender;
 
     @Override
     public void sendInvitationMail(InvitationRequest invitationRequest) {
+        SimpleMailMessage message = new SimpleMailMessage();
+
+        message.setTo(invitationRequest.getEmail());
+        message.setSubject(null);
+        message.setText(invitationRequest.getUrl());
+        this.javaMailSender.send(message);
+
         logger.info("Invite email sent to : " + invitationRequest.getEmail());
     }
 }
